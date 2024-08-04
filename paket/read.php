e7eff77cb170e7233f4d4578ec3c602857c49bd7
mrisqikhasani <mrisqikhasani@gmail.com>
2024-08-04 12:49:48 +0700

    hapus cek admin pada paket/read.php

--- paket/read.php
+++ paket/read.php
@@ -3,7 +3,7 @@ session_start();
 
 // memeriksa apakah pengguna sudah login dan memiliki peran "admin
 
-if(isset($_SESSION['user_role']) && $_SESSION['user_role'] == 'admin'){
+// if(isset($_SESSION['user_role']) && $_SESSION['user_role'] == 'admin'){
 
     header('Access-Control-Allow-Origin:*');
     header('Content-Type: application/json');
@@ -35,14 +35,15 @@ if(isset($_SESSION['user_role']) && $_SESSION['user_role'] == 'admin'){
         header("HTTP/1.0 405 Method Not Allowed");
         echo json_encode($data);
     }
-} else {
-    // Pengguna tidak memiliki akses ke halaman ini
-    $data = [
-        'status' => 403,
-        'message' => 'Forbidden: You do not have access to this page',
-    ];
-    header("HTTP/1.0 403 Forbidden");
-    echo json_encode($data);
-    exit;
-}
+// } else {
+//     // Pengguna tidak memiliki akses ke halaman ini
+//     $data = [
+//         'status' => 403,
+//         'message' => 'Forbidden: You do not have access to this page',
+//     ];
+//     header("HTTP/1.0 403 Forbidden");
+//     echo json_encode($data);
+//     exit;
+// }
+
 ?>
